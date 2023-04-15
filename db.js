5a2c7c77098846dd9901e96d10ba2ea7a0acda73
Dev Julian <dev.achmadjulian@gmail.com>
2023-04-15 09:29:58 +0700

    fix uri mongodb atlas

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://guest_bookstoreapp:guest000@books-store-cluster.ut8xwjm.mongodb.net/bookstore?retryWrites=true&w=majority";
+const uri = "mongodb+srv://guest_bookstoreapp:guest000@books-store-cluster.ut8xwjm.mongodb.net/?retryWrites=true&w=majority";
 let dbConnection;
 
 module.exports = {
